6ee7a35f061f340724126497c37f17ddf82620d0
Candle779 <62902629+Candle779@users.noreply.github.com>
2020-10-26 00:08:23 +0300

    fix: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = [];
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  bracketsConfig.forEach((item) => {
+    openBrackets.push(item[0]);
+    closeBrackets.push(item[1]);
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    if (closeBrackets.includes(str[i]) && arr.length !== 0) {
+      const correspondBracket = openBrackets[closeBrackets.indexOf(str[i])];
+      if (arr[arr.length - 1] === correspondBracket || arr[arr.length - 1] === str[i]) {
+        arr.pop();
+      } else if (str[i] !== correspondBracket) {
+        return false;
+      } else {
+        arr.push(str[i]);
+      }
+    } else {
+      arr.push(str[i]);
+    }
+  }
+  return arr.length === 0;
+};
